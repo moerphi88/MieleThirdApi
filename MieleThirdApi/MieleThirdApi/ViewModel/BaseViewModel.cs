2c73e841dbf976b58025fc8dba78f77b8a173e74
johannes watermann <moerphi@gmx.de>
2019-06-20 22:39:53 +0200

    Anscheinend muss ich doch OnPropertyChanged für die ObservableCollection machen.

--- MieleThirdApi/MieleThirdApi/ViewModel/BaseViewModel.cs
+++ MieleThirdApi/MieleThirdApi/ViewModel/BaseViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using MieleThirdApi.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 
@@ -6,13 +9,31 @@ namespace MieleThirdApi.ViewModels
 {
     class BaseViewModel : INotifyPropertyChanged
     {
-        protected INavigation _navigation { get; }   
-
+        protected INavigation _navigation { get; }
+        protected IRestApi _restApi { get; }
+             
         protected BaseViewModel(INavigation navigation)
         { 
             _navigation = navigation;
+
+            //Hier kann später die Compiler MOCK Abfrage rein
+            _restApi = new RestMockService();
         }
-        
+
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName]string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
         #region INotifyPropertyChanges Handler
 
         public event PropertyChangedEventHandler PropertyChanged;
