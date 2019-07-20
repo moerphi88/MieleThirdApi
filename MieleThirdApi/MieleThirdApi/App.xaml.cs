c5d218b5a984e6273369221b0791fdb76e2fc9d2
moerphi88 <moerphi@gmx.de>
2019-07-20 13:55:14 +0200

    added navigation page

--- MieleThirdApi/MieleThirdApi/App.xaml.cs
+++ MieleThirdApi/MieleThirdApi/App.xaml.cs
@@ -12,7 +12,7 @@ namespace MieleThirdApi
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new MainPage());
         }
 
         protected override void OnStart()
