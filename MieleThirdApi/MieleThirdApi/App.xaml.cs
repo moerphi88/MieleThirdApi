df01355d79f8f792ed50b76b5245e5ea3f21d225
moerphi88 <moerphi@gmx.de>
2019-07-24 18:39:13 +0200

    added debug log outputs

--- MieleThirdApi/MieleThirdApi/App.xaml.cs
+++ MieleThirdApi/MieleThirdApi/App.xaml.cs
@@ -18,7 +18,7 @@ namespace MieleThirdApi
             //System.Diagnostics.Debug.WriteLine($"Zeitmessung {App.watch.ElapsedMilliseconds} ms");
             InitializeComponent();
 
-            Debug.WriteLine("Sowieso");
+            //Debug.WriteLine("Sowieso");
             LoginManager = LoginManager ?? new LoginMockManager();
             //(LoginManager = LoginManager ?? new LoginManager();
 
@@ -36,17 +36,20 @@ namespace MieleThirdApi
         protected override void OnStart()
         {
             // Handle when your app starts
+            Debug.WriteLine("OnStart");
         }
 
         protected override void OnSleep()
         {
             // Handle when your app sleeps
+            Debug.WriteLine("OnSleep");
             watch.Stop();
         }
 
         protected override void OnResume()
         {
             // Handle when your app resumes
+            Debug.WriteLine("OnResume");
         }
     }
 }
